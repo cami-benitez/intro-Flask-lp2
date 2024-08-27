026fa8c0a93e6518c68ccbfdb9152f943027e4e2
cami benitez <camila.benitez0021@gmail.com>
2024-08-27 18:26:14 -0400

    add: cambio-ciudades

--- conexion/conexion.py
+++ conexion/conexion.py
@@ -1,9 +1,9 @@
 import psycopg2 
-class conexion:
+class Conexion:
     """Metodo constructor de tu perro
     """
     def __init__(self):
-        self.con = psycopg2.connect("dbname=veterinaria-db user=postgres host=localhost password=6740362")
+        self.con = psycopg2.connect("dbname=veterinaria-db user=juandba host=localhost password=admin")
         """getConexion 
         self
         retorno la instalacion de la base de datos 
